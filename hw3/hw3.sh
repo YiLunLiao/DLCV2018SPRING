9c3790ad29f1072ff8863cbf047497c8947c1297
root <b03901001@ntu.edu.tw>
2018-05-04 23:48:11 +0800

    new file:   hw3.sh 	new file:   hw3_B03901001.pdf 	new file:   hw3_best.sh 	new file:   hw3_test.py

---
+++ hw3/hw3.sh
@@ -0,0 +1,2 @@
+wget https://www.dropbox.com/s/xrariv08gbpvslw/vgg32s.h5?dl=1 -O vgg32s.h5
+python3 hw3_test.py vgg32s.h5 $1 $2
---
+++ hw3/hw3_best.sh
@@ -0,0 +1,2 @@
+wget https://www.dropbox.com/s/0h6alotqeoc44w2/vgg16s_cat64.h5?dl=1 -O vgg16s_cat64.h5
+python3 hw3_test.py vgg16s_cat64.h5 $1 $2
